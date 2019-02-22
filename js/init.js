1cd1b6a826ec3c8e1796d46f4162738f6668664c
Mario Carranza <mario.carranza996@gmail.com>
2019-02-22 15:26:23 -0600

    Datatables diseño

--- js/init.js
+++ js/init.js
@@ -8,6 +8,27 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
 
     $('#example').DataTable({
-        "scrollX":true
+        "scrollX":true,
+        columnDefs: [
+            {
+                targets: [ 0, 1, 2 ],
+                className: 'mdl-data-table__cell--non-numeric'
+            }
+        ],
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros por página",
+            "search": "Buscar:",
+            "zeroRecords": "No hay registros",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "No hay registros disponibles",
+            "infoFiltered": "(filtered from _MAX_ total records)",
+            "paginate": {
+                "first":      "Primero",
+                "last":       "Último",
+                "next":       "Siguiente",
+                "previous":   "Anterior"
+            },
+        }
     });
 });
